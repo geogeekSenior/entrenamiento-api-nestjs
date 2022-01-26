85e0f83895edb29550679714f3364c97c9f2222d
Andres Loto <riderandresj@hotmail.com>
2022-01-26 01:11:42 -0500

    Se crean los modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { CursoModule } from './curso/curso.module';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, CursoModule, UsuarioModule],
   controllers: [AppController],
   providers: [AppService],
 })
