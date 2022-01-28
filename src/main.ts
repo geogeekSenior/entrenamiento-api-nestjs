c9150bda5fb5d4dadeb48919f2f7d5f4f6432c6f
Andres Loto <riderandresj@hotmail.com>
2022-01-28 17:48:01 -0500

    cambios de entorno

--- src/main.ts
+++ src/main.ts
@@ -24,5 +24,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
   await app.listen(3000);
+  console.log(`Enviroment ${process.env.NODE_ENV}`);
+  console.log(`Servidor corriendo en puerto`);
 }
 bootstrap();
