0271287a8fc245467d1e330935f9da8318dddbac
geogeeks <geogeekssig@gmail.com>
2022-02-08 00:34:03 -0500

    Configurando imports y exports

--- src/curso/curso.module.ts
+++ src/curso/curso.module.ts
@@ -1,4 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Certificado } from './entities/certificado.entity';
+import { Clase } from './entities/clase.entity';
+import { Curso } from './entities/curso.entity';
+import { Empresa } from './entities/empresa.entity';
+import { Encuesta } from './entities/encuesta.entity';
+import { Grupo } from './entities/grupo.entity';
+import { Horario } from './entities/horario.entity';
+import { Pregunta } from './entities/pregunta.entity';
+import { Salon } from './entities/salon.entity';
+import { Sector } from './entities/sector.entity';
+import { Ubicacion } from './entities/ubicacion.entity';
 
-@Module({})
-export class CursoModule {}
+@Module({
+    imports: [TypeOrmModule.forFeature([
+        Certificado,
+        Clase,
+        Curso,
+        Empresa,
+        Encuesta,
+        Grupo,
+        Horario,
+        Pregunta,
+        Salon,
+        Sector,
+        Ubicacion,
+    ])],
+    exports: [],
+})
+export class CursoModule { }
--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { EstudianteService } from './services/estudiante.service';
 import { EstudianteController } from './controllers/estudiante.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Estudiante } from './entities/estudiante.entity';
+import { CursoModule } from './../curso/curso.module';
+import { Instructor } from './entities/instructor.entity';
 
 @Module({
+  imports: [CursoModule,TypeOrmModule.forFeature([Estudiante, Instructor])],
   providers: [EstudianteService],
   controllers: [EstudianteController]
 })
-export class UsuarioModule {}
+export class UsuarioModule { }
